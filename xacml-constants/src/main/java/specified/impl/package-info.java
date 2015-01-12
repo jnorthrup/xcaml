5d38e2a3b41ab2e8a1170d5d795f0d0c4db12951
Northrup, James (jn218y) <jn218y@us.att.com>
2015-01-12 02:46:50 -0800

    tweak Attribute

--- xacml-constants/src/main/java/specified/impl/package-info.java
+++ xacml-constants/src/main/java/specified/impl/package-info.java
@@ -1,8 +1,10 @@
 /**
- *
+ * 
  * if {@link specified.XacmlFunctionProto} enum elements compose a .h file, then this package provides .cxx for the
  * compiler to bind functions to those function prototypes.
- *
- * the first time a {@link specified.XacmlFunctionProto} is called and executes {@link specified.XacmlFunctionProto#apply}, the registration of that element iterates this package for the first matching {@link specified.BindXacmlFunctions} or {@link specified.BindUri} element to provide apply()
+ * 
+ * the first time a {@link specified.XacmlFunctionProto} is called and executes
+ * {@link specified.XacmlFunctionProto#apply}, the registration of that element iterates this package for the first
+ * matching {@link specified.BindXacmlFunctions} or {@link specified.BindUri} element to provide apply()
  */
 package specified.impl;
