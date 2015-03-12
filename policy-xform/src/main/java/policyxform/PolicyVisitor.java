a91d5dd321dc22c509230891e19ef413f707d9b6
Jim Northrup <northrup.james@gmail.com>
2015-03-12 01:14:54 -0700

    produce self-introspecting description text in trace

--- policy-xform/src/main/java/policyxform/PolicyVisitor.java
+++ policy-xform/src/main/java/policyxform/PolicyVisitor.java
@@ -3,6 +3,9 @@ package policyxform;
 import com.google.common.base.Joiner;
 import oasis.names.tc.xacml._3_0.core.schema.wd_17.*;
 import org.apache.camel.schema.spring.CamelContextElement;
+import org.apache.camel.schema.spring.DescriptionElement;
+import org.apache.camel.schema.spring.ObjectFactory;
+import org.apache.camel.schema.spring.RouteElement;
 import org.springframework.schema.beans.Beans;
 import org.springframework.schema.beans.Description;
 import org.w3c.dom.Document;
@@ -15,96 +18,197 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PolicyVisitor {
 
-  interface Parent {
-  };
+    public static final Class[] COMMON_CLASSES = new Class[]{
+            org.springframework.schema.beans.ObjectFactory.class, ObjectFactory.class,
+            oasis.names.tc.xacml._3_0.core.schema.wd_17.ObjectFactory.class};
+    long c = 0;
+    JAXBContext currentJaxbContext;
+    private CamelContextElement currentCamelContext;
+    private Beans beans;
+    private PolicyType currentPolicy;
+    private PolicySetType currentPolicySet;
+    private RouteElement currentRoutElement;
 
-  interface Node {
-  }
+    ;
 
-  long c = 0;
-
-  public static void main(String... args) throws InterruptedException,
-      ParserConfigurationException, IOException, SAXException, JAXBException {
-    new PolicyVisitor().visitXml(args);
-  }
-
-  public void visitXml(String... args) throws JAXBException, ParserConfigurationException,
-      IOException, SAXException, InterruptedException {
-    JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
-    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-    documentBuilderFactory.setNamespaceAware(true);
-    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-
-    final String infile = args[0];
-    Path path1 = Paths.get(infile);
-    byte[] input = Files.readAllBytes(path1);
-    Document doc =
-        documentBuilder.parse(new ByteArrayInputStream(new String(input).replaceAll("\\s+", " ")
-            .getBytes(StandardCharsets.UTF_8)));
-    Unmarshaller unmarshaller = jc.createUnmarshaller();
-    Element documentElement = doc.getDocumentElement();
-    String tagName = documentElement.getTagName();
-    System.err.println("" + tagName);
-    final String out = args[1];
-    CamelContextElement parent = new CamelContextElement().withId(id());
-    switch (tagName) {
-      case "PolicySet": {
-        JAXBElement<PolicySetType> e = unmarshaller.unmarshal(doc, PolicySetType.class);
-        visitPolicySet(e, parent);
-        break;
-      }
-
-      case "Policy": {
-        JAXBElement<PolicyType> e = unmarshaller.unmarshal(doc, PolicyType.class);
-        visitPolicy(e, parent);
-        break;
-      }
-      default:
-        System.exit(1);
+    public PolicyVisitor() throws JAXBException {
+        currentJaxbContext = createJaxbContext();
     }
-    writeGraph(new Beans().withImportOrAliasOrBean(parent).withDescription(
-        new Description().withContent("beans::foo " + id())), out);
-  }
 
-  public void visitPolicy(JAXBElement<PolicyType> e, Object parent, Object... createEdgeToHere) {
+    public static void main(String... args) throws InterruptedException,
+            ParserConfigurationException, IOException, SAXException, JAXBException {
+        new PolicyVisitor().visitXml(args);
+    }
 
-  }
+    static String traceDescriptionElement(int... skip) {
+        int sk = skip.length < 1 ? 2 : skip[0];
+        final Iterator<StackTraceElement> x =
+                Arrays.asList(Thread.currentThread().getStackTrace()).stream().skip(sk).limit(1).iterator();
+        String value = null;
+        while (x.hasNext()) {
+            StackTraceElement next = x.next();
+            final String format =
+                    MessageFormat.format("{0} ({1}:{2} )", next.getMethodName(), next.getFileName(), next
+                            .getLineNumber());
+            value = (format);
+            break;
+        }
+        return value;
+    }
 
-  public void visitPolicySet(JAXBElement<PolicySetType> e, Object graph, Object... createEdgeToHere) {
+    static JAXBContext createJaxbContext(Class... classes) throws JAXBException {
+        return JAXBContext.newInstance(classes.length == 0 ? COMMON_CLASSES : classes);
+    }
 
-  }
+    public void visitXml(String... args) throws JAXBException, ParserConfigurationException,
+            IOException, SAXException, InterruptedException {
+        JAXBContext jc =
+                JAXBContext.newInstance(oasis.names.tc.xacml._3_0.core.schema.wd_17.ObjectFactory.class);
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        documentBuilderFactory.setNamespaceAware(true);
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
 
-  public Node visitAdviceExpressions(Parent outer, AdviceExpressionsType adviceExpressions) {
-    return null;
-  }
+        final String infile = args[0];
+        Path path1 = Paths.get(infile);
+        byte[] input = Files.readAllBytes(path1);
+        Document doc =
+                documentBuilder.parse(new ByteArrayInputStream(new String(input).replaceAll("\\s+", " ")
+                        .getBytes(StandardCharsets.UTF_8)));
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        Element documentElement = doc.getDocumentElement();
+        String tagName = documentElement.getTagName();
+        System.err.println("" + tagName);
+        final String out = args[1];
+        currentCamelContext = new CamelContextElement();
+        switch (tagName) {
+            case "PolicySet": {
+                JAXBElement<PolicySetType> e = unmarshaller.unmarshal(doc, PolicySetType.class);
+                currentCamelContext.setId(e.getValue().getPolicySetId());
+                visitPolicySet(e, currentCamelContext);
+                break;
+            }
 
-  public Node visitObligationExpressions(Parent outer,
-      ObligationExpressionsType ObligationExpressions) {
-    return null;
-  }
+            case "Policy": {
+                JAXBElement<PolicyType> e = unmarshaller.unmarshal(doc, PolicyType.class);
+                currentCamelContext.setId(e.getValue().getPolicyId());
+                visitPolicy(e, currentCamelContext);
+                break;
+            }
+            default:
+                System.exit(1);
+        }
+        beans =
+                new Beans().withImportOrAliasOrBean(currentCamelContext).withDescription(
+                        new Description().withContent("beans::foo " + id()));
+        writeGraph(beans, out);
+    }
 
-  public Node visitRule(Node topLink, Parent c, Node prevLink, RuleType ruleType) {
+    public void visitPolicy(JAXBElement<PolicyType> e, Object... createEdgeToHere)
+            throws JAXBException {
+        currentPolicy = e.getValue();
+        String policySetId = currentPolicy.getPolicyId();
+        currentCamelContext.setId(policySetId);
+        currentPolicy.getRuleCombiningAlgId();
+        // final RouteElement routeElement = new RouteElement().withDescription(traceDescriptionElement( ));
 
-    return null;
-  }
+        currentRoutElement = new RouteElement().withDescription(new DescriptionElement()
+                        .withValue(
+                                currentPolicy .getDescription()+
+                                        System.lineSeparator() +
+                                        "+trace.out: " +
+                                        traceDescriptionElement())
+        );
+        currentCamelContext.getRoute().add(currentRoutElement);
+    }
 
-  public Object visitTarget(Parent graph, Object parent, TargetType targetType) {
-    Object val = null;
+    public StringWriter reifyProxy() throws JAXBException {
+        final StringWriter writer = new StringWriter();
+        final Marshaller marshaller = createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+        marshaller.marshal(currentPolicy, writer);
+        return writer;
+    }
 
-    return val;
-  }
+    public void visitPolicySet(JAXBElement<PolicySetType> e, CamelContextElement graph,
+                               Object... createEdgeToHere) {
+        currentPolicySet = e.getValue();
+        String policySetId = currentPolicySet.getPolicySetId();
+        graph.setId(policySetId);
 
-  public String getContentString(List<Object> content) {
+        final StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        final Object[] objects =
+                Arrays.asList(stackTrace).stream().skip(1).limit(1).distinct().toArray();
+
+        // Node policySet = new Node().withId(id()).withLabel("PolicySet:" + urnTip(policySetId));
+        // addChildToFirstOf(policySet, graph);
+        // linkChildToFirstOf(graph, policySet, createEdgeToHere);
+        // System.err.println("PolicySet: " + (top).toString());
+        //
+        // Object c = graph;
+        // visitTarget(graph, policySet, top.getTarget());
+        // final AdviceExpressionsType adviceExpressions = top.getAdviceExpressions();
+        // Node child = visitAdviceExpressions(c, adviceExpressions);
+        //
+        // linkChildToFirstOf(graph, child,/* fnNode,policyNode, */createEdgeToHere);
+        // child = visitObligationExpressions(c, top.getObligationExpressions());
+        // linkChildToFirstOf(graph, child,/* fnNode,policyNode, */createEdgeToHere);
+        //
+        // List<JAXBElement<?>> policySetOrPolicyOrPolicySetIdReference =
+        // top.getPolicySetOrPolicyOrPolicySetIdReference();
+        //
+        // for (JAXBElement<?> jaxbElement : policySetOrPolicyOrPolicySetIdReference)
+        // switch (jaxbElement.getValue().getClass().getSimpleName()) {
+        // case "PolicySetType":
+        // visitPolicySet((JAXBElement<PolicySetType>) jaxbElement, c, policySet);
+        // break;
+        // case "PolicyType":
+        // visitPolicy((JAXBElement<PolicyType>) jaxbElement, c, policySet);
+        // break;
+        // case "PolicySetIdReference":
+        // case "PolicyIdReference":
+        // default:
+        // System.err.println("not handling " + jaxbElement.getValue().getClass().getSimpleName());
+        // break;
+        // }
+
+    }
+
+    public Node visitAdviceExpressions(Parent outer, AdviceExpressionsType adviceExpressions) {
+        return null;
+    }
+
+    public Node visitObligationExpressions(Parent outer,
+                                           ObligationExpressionsType ObligationExpressions) {
+        return null;
+    }
+
+    public Node visitRule(Node topLink, Parent c, Node prevLink, RuleType ruleType) {
+
+        return null;
+    }
+
+    public Object visitTarget(Parent graph, Object parent, TargetType targetType) {
+        Object val = null;
+
+        return val;
+    }
+
+    public String getContentString(List<Object> content) {
         String join = null;
         Joiner contentsJoined = Joiner.on(",");
         try {
@@ -115,60 +219,72 @@ public class PolicyVisitor {
         return join;
     }
 
-  String hashTip(String dataType1) {
-    char ch = '#';
-    return urnTip(stringTip(dataType1, ch));
-  }
-
-  String urnTip(String attributeId) {
-    char ch = ':';
-    return stringTip(attributeId, ch);
-  }
-
-  String stringTip(String dataType1, char ch) {
-
-    final int beginIndex = dataType1.lastIndexOf(ch);
-    if (-1 == beginIndex)
-      return dataType1;
-    return dataType1.substring(beginIndex);
-  }
-
-  public String id() {
-    return "n" + c++;
-  }
-
-  String getId(Object reflectme) {
-    try {
-      return String.valueOf(reflectme.getClass().getMethod("getId").invoke(reflectme));
-    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-      e.printStackTrace();
+    String hashTip(String dataType1) {
+        char ch = '#';
+        return urnTip(stringTip(dataType1, ch));
     }
-    return null;
-  }
 
-  void linkChildToFirstOf(Parent graph, Object to, Object... from) {
-  }
+    String urnTip(String attributeId) {
+        char ch = ':';
+        return stringTip(attributeId, ch);
+    }
 
-  void addChildToFirstOf(Object child, Object... parent) {
+    String stringTip(String dataType1, char ch) {
 
-  }
+        final int beginIndex = dataType1.lastIndexOf(ch);
+        if (-1 == beginIndex)
+            return dataType1;
+        return dataType1.substring(beginIndex);
+    }
 
-  Object visitExpression(Parent outer, JAXBElement<?> expression) {
+    public String id() {
+        return "n" + c++;
+    }
+
+    String getId(Object reflectme) {
+        try {
+            return String.valueOf(reflectme.getClass().getMethod("getId").invoke(reflectme));
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    void linkChildToFirstOf(Parent graph, Object to, Object... from) {
+    }
+
+    void addChildToFirstOf(Object child, Object... parent) {
+
+    }
+
+    Object visitExpression(Parent outer, JAXBElement<?> expression) {
     /*
      * possible object is AttributeValueType AttributeDesignatorType VariableReferenceType AttributeSelectorType
      * FunctionType ApplyType ExpressionType
      */
-    return null;
-  }
+        return null;
+    }
 
-  public void writeGraph(Beans graph, String out) throws JAXBException, IOException,
-      InterruptedException {
-    final JAXBContext jaxbContext = JAXBContext.newInstance(Beans.class, CamelContextElement.class);
+    public void writeGraph(Beans graph, String out) throws JAXBException, IOException,
+            InterruptedException {
+        final Marshaller marshaller = createMarshaller(COMMON_CLASSES);
+        // output pretty printed
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(graph, Paths.get(out + ".spring.xml").toFile());
+        marshaller.marshal(graph, System.out);
 
-    final Marshaller marshaller = jaxbContext.createMarshaller();
-    marshaller.marshal(graph, Paths.get(out + ".spring.xml").toFile());
-    marshaller.marshal(graph, System.out);
+    }
 
-  }
+    Marshaller createMarshaller(Class... classes) throws JAXBException {
+        final JAXBContext jaxbContext = currentJaxbContext;
+
+        return jaxbContext.createMarshaller();
+    }
+
+    interface Parent {
+    }
+
+    interface Node {
+    }
 
 }
